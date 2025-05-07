9b81b4df4c51a98786dc38c776fbf04435b38243
Islam Ahmed <eslamelbltagye1111@gmail.com>
2025-05-07 22:52:17 +0300

    [219] [Time Beats: 17.25%] [Memory Beats: 7.18%] - LeetPush

--- Contains-Duplicate-II.cpp
+++ Contains-Duplicate-II.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        map<int, int> hash;
+        set<int> window;
 
         for (int i = 0; i < nums.size(); i++) {
-            int j = nums[i];
-            if (hash.find(j) != hash.end()) {
-                int idx = hash[j];
-                int sub = -(idx - i);
-                if (sub <= k) {
-                    return true;
-                }
+            if (window.count(nums[i])) {
+                return true;
             }
 
-            hash[j] = i;
+            window.insert(nums[i]);
+
+            if (window.size() > k) {
+                window.erase(nums[i - k]);
+            }
         }
 
         return false;
