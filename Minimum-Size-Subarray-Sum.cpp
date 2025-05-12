b0878084462e112e5f4b6e39485477e92bdde65d
Islam Ahmed <eslamelbltagye1111@gmail.com>
2025-05-12 22:48:29 +0300

    [209] [Time Beats: 100.00%] [Memory Beats: 39.86%] - LeetPush

---
+++ Minimum-Size-Subarray-Sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int l = 0;
+        int sum = 0;
+        int minLength = INT_MAX;
+
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+            while (sum >= target) {
+                minLength = min(minLength, i - l +1);
+                sum -= nums[l];
+                l+= 1;
+            }
+        }
+
+        return minLength != INT_MAX ? minLength : 0;
+
+    }
+};
